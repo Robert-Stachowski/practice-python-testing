536358b83a311d98fdc64ca26f3bfaf997878411
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-09 15:35:25 +0200

    Add validate_order function

--- 01/orders.py
+++ 01/orders.py
@@ -1,6 +1,22 @@
 def validate_order(order):
     if not isinstance(order.get("user_id"), int):
         raise ValueError("Nieprawidłowy user_id")
-    # uzupełnij resztę przypadków
+    
+    items = order.get("items")
+    if not isinstance(items, list) or len(items)==0:
+        raise ValueError("Lista pusta")
+    
+    delivery = order.get("delivery")
+    if not isinstance(delivery, dict):
+        raise ValueError("niepoprawny format")
+    
+    allowed_method = ("courier", "pickup", "drone")
+    delivery_methods = delivery.get("method")
+    if delivery_methods not in allowed_method:
+        raise ValueError(" Nieprawidłowa metoda")
+    
+    delivery_address = delivery.get("address")
+    if not isinstance(delivery_address, str) or not delivery_address.strip():
+        raise ValueError("Brak lub nieprawidłowy adres")
 
     return True
