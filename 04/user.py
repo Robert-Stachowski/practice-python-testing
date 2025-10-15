3958acdc3d1f89d1f0b0da96fc1ae1a0c50fcd72
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-15 13:03:12 +0200

    Add complete validate_user() fuction

--- 04/user.py
+++ 04/user.py
@@ -1,3 +1,11 @@
 def validate_user(data):
-    
+    email = data.get("email","")
+    if "@" not in email or not data.get("email").endswith((".pl", ".com")):
+        return False
+    age = data.get("age")
+    if not isinstance(age, int) or age <18:
+        return False
+    name = data.get("name","")
+    if not name.isalpha():
+        return False
     return True
