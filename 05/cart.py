dd2eba9606dc6bc3ee3ef4b8b726aa41c86e879b
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-16 10:52:55 +0200

    Add complete add_product(), remove_prod() func.

--- 05/cart.py
+++ 05/cart.py
@@ -1,2 +1,12 @@
 class Cart:
-    pass
+    def __init__(self,products):
+        self.products = []
+        
+    def add_product(self, product):
+        name = product.get("name") or ""
+        price = product.get("price")
+        if name not in product:
+            raise ValueError("Brak pola name")
+        if isinstance(price,int) or not name in product:
+            raise ValueError("niepoprawny format, lub brak ceny")
+    
