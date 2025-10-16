3455d356680b67496f783b5be40293c06c9d6fde
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-16 15:42:49 +0200

    Add complete tests

--- 05/test_cart.py
+++ 05/test_cart.py
@@ -15,13 +15,16 @@ def cart_with_items():
     cart.add_product({"name": "apple", "price": 3.8})
     return cart
 
+
+
 def test_add_product(cart_with_items):
     len_before = len(cart_with_items.items())
     new_product = {"name": "orange", "price": 8}
     cart_with_items.add_product(new_product)
     len_after = len(cart_with_items.items())
     assert len_after == len_before+1
-
+    assert new_product in cart_with_items.items()
+    
 
 def test_remove_product(cart_with_items):
     len_before = len(cart_with_items.items())
@@ -32,7 +35,9 @@ def test_remove_product(cart_with_items):
     expected_names = {"banana","apple","kiwi"}
     names_after_remove = {p["name"] for p in cart_with_items.items()}
     assert names_after_remove == expected_names
-    
+    # liczenie ile "jabłek" zostało w koszyku. (były dwa pierwotnie)
+    assert [p["name"] for p in cart_with_items.items()].count("apple") == 1
+
 
 def test_total_price(cart_with_items):
     expected_total = 17.6
@@ -40,11 +45,13 @@ def test_total_price(cart_with_items):
     assert result == expected_total
     assert isinstance(result, float)
 
+
 def test_empty_cart_total(empty_cart):
-    expected_total = 0
+    expected_total = 0.0
     result = empty_cart.total_price()
     assert result == expected_total
     assert isinstance(result, float)
+
 
 def test_remove_nonexistent_product(empty_cart):
     len_before = len(empty_cart.items())
@@ -54,3 +61,12 @@ def test_remove_nonexistent_product(empty_cart):
     items_after = empty_cart.items()
     assert len_after == len_before
     assert items_before == items_after
+
+
+def test_items_deepcopy(cart_with_items):
+    total_before = cart_with_items.total_price()
+    copied_list = cart_with_items.items()
+    copied_list[0]["price"] = 10
+    total_after = cart_with_items.total_price()
+    assert total_before == total_after
+    assert copied_list is not cart_with_items.items()
