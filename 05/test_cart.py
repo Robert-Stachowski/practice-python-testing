e5080c847c30da8a1bfa184b9b2e74a91a1cdb81
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-16 13:01:07 +0200

    Add @pytest.fixture Cart() products & add_poduct,remove_product tests

--- 05/test_cart.py
+++ 05/test_cart.py
@@ -1,17 +1,40 @@
 import pytest
 from cart import Cart
 
-def test_add_product():
+@pytest.fixture
+def empty_cart():
+    cart = Cart()
+    return cart
+
+@pytest.fixture
+def cart_with_items():
+    cart = Cart()
+    cart.add_product({"name": "apple", "price": 3.5})
+    cart.add_product({"name": "banana", "price": 4})
+    cart.add_product({"name": "kiwi", "price": 6.3})
+    cart.add_product({"name": "apple", "price": 3.8})
+    return cart
+
+def test_add_product(cart_with_items):
+    len_before = len(cart_with_items.items())
+    new_product = {"name": "orange", "price": 8}
+    cart_with_items.add_product(new_product)
+    len_after = len(cart_with_items.items())
+    assert len_after == len_before+1
+
+
+def test_remove_product(cart_with_items):
+    len_before = len(cart_with_items.items())
+    cart_with_items.remove_product("apple")
+    len_after = len(cart_with_items.items())
+    assert len_after == len_before -1
+    
+
+def test_total_price(cart_with_items):
     pass
 
-def test_remove_product():
+def test_empty_cart_total(empty_cart):
     pass
 
-def test_total_price():
-    pass
-
-def test_empty_cart_total():
-    pass
-
-def test_remove_nonexistent_product():
+def test_remove_nonexistent_product(empty_cart):
     pass
