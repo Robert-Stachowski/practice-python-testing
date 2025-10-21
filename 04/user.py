f038ac2bdfabcad7f276b8b5d973ea919f802b0a
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-21 13:58:16 +0200

    refactor(user): full data validation with key checks and clean test coverage

--- 04/user.py
+++ 04/user.py
@@ -1,15 +1,26 @@
 def validate_user(data):
+    
+    if not isinstance(data,dict):
+        return False
+    
+    required_keys = {"email","age","name"}
+    if not required_keys.issubset(data):
+        return False
+    
     email = data.get("email")
     if not isinstance(email, str) or email != email.strip():
         return False
     if "@" not in email or not email.endswith((".pl", ".com")):
         return False
+    
     age = data.get("age")
     if not isinstance(age, int) or age <18:
         return False
+    
     name = data.get("name")
     if not isinstance(name, str) or name != name.strip():
         return False
     if not name.isalpha():
         return False
+    
     return True
